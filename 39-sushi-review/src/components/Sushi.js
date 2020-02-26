1c77ab3cc7a2a90607f6f719ee091e17afd2cabd
vibraniumforge <gregm9999@gmail.com>
2020-02-25 17:09:41 -0800

    removed linting errors

--- 39-sushi-review/src/components/Sushi.js
+++ 39-sushi-review/src/components/Sushi.js
@@ -1,26 +1,18 @@
-import React, { Fragment } from 'react'
+import React from "react";
 
-const Sushi = ({name, img_url, id, price, eatSushi, eaten}) => {
-  // const eatSushi = () => {
-
-  // }
+const Sushi = ({ name, img_url, id, price, eatSushi, eaten }) => {
   return (
     <div className="sushi">
-      <div className="plate" 
-           onClick={() => eatSushi(id, price)}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          eaten ?
-            null
-          :
-            <img src={img_url} width="100%" />
-        }
+      <div className="plate" onClick={() => eatSushi(id, price)}>
+        {/* Tell me if this sushi has been eaten! */
+
+        eaten ? null : <img src={img_url} alt="sushi" width="100%" />}
       </div>
       <h4 className="sushi-details">
         {name} - ${price}
       </h4>
     </div>
-  )
-}
+  );
+};
 
-export default Sushi
+export default Sushi;
--- 39-sushi-review/src/containers/Table.js
+++ 39-sushi-review/src/containers/Table.js
@@ -1,32 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const Table = ({balance, eatenSushis}) => {
-
-  const renderPlates = (array) => {
+const Table = ({ balance, eatenSushis }) => {
+  const renderPlates = array => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
-    })
-  }
+      return (
+        <div key={index} className="empty-plate" style={{ top: -7 * index }} />
+      );
+    });
+  };
 
   return (
     <Fragment>
-      <h1 className="remaining">
-        You have: ${ balance } remaining!
-      </h1>
+      <h1 className="remaining">You have: ${balance} remaining!</h1>
       <div className="table">
         <div className="stack">
-          {
-            /* 
+          {/* 
                renderPlates takes an array 
                and renders an empty plate
                for every element in the array
             */
-            renderPlates(eatenSushis)
-          }
+          renderPlates(eatenSushis)}
         </div>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
